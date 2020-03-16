8e4932b10e6ee1fad9babb91a1b9faa8c981fb4c
Xun Li <xunli@uchicago.edu>
2020-03-16 15:33:40 -0700

    Update uiManager.js

--- media/js/ui/uiManager.js
+++ media/js/ui/uiManager.js
@@ -185,7 +185,7 @@ var UIManager = (function(window, csrftoken, $, MsgBox ){
 
     $(window).resize(function() {
       $('#layer-tree, .tool-menu').hide();
-      $('#dialog-open-file').dialog("option","position","center");
+      //$('#dialog-open-file').dialog("option","position","center");
     });
 
     var local_map_names_sel = {
